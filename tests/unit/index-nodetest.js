176c54d0321678ee646d8ebbca7460e57ca4088c
Phil Gengler <phil@pgengler.net>
2017-01-01 20:07:32 -0500

    Update tests

--- tests/unit/index-nodetest.js
+++ tests/unit/index-nodetest.js
@@ -30,7 +30,7 @@ var mockSSHClient = CoreObject.extend({
     });
   },
 
-  _uploadedFiles: {},
+  _uploadedFiles: { },
 
   upload: function(path, data) {
     var files = this._uploadedFiles;
@@ -44,7 +44,7 @@ var mockSSHClient = CoreObject.extend({
   putFile: function(src, dest) {
     var files = this._uploadedFiles;
 
-    var file = fs.readFileSync(src, "utf8");
+    var file = fs.readFileSync(src, 'utf8');
 
     return new Promise(function(resolve) {
       files[dest] = file.toString();
@@ -76,7 +76,7 @@ describe('the deploy plugin object', function() {
     var subject = require('../../index');
 
     plugin = subject.createDeployPlugin({
-      name: 'ssh2',
+      name: 'with-rsync',
     });
 
     context = {
@@ -85,12 +85,11 @@ describe('the deploy plugin object', function() {
         'ssh2': {
           username: 'deployer',
           password: 'mypass',
-          applicationFiles: ['index.html', 'manifest.appcache'],
           root: '/usr/local/www/my-app',
           distDir: 'tests/fixtures/dist',
           revisionMeta: function() {
             var revisionKey = this.readConfig('revisionKey');
-          
+
             return {
               revision: revisionKey,
             };
@@ -109,7 +108,7 @@ describe('the deploy plugin object', function() {
   });
 
   it('has a name', function() {
-    assert.equal('ssh2', plugin.name);
+    assert.equal('with-rsync', plugin.name);
   });
 
   it('implements the correct hooks', function() {
@@ -123,7 +122,7 @@ describe('the deploy plugin object', function() {
         .then(function() {
           var client = plugin._client;
 
-          assert.equal(client._connected, true); 
+          assert.equal(client._connected, true);
         });
     });
 
@@ -132,39 +131,39 @@ describe('the deploy plugin object', function() {
         .then(function() {
           var client = plugin._client;
 
-          assert.equal(client.options.username, "deployer"); 
-          assert.equal(client.options.password, "mypass");         
+          assert.equal(client.options.username, 'deployer');
+          assert.equal(client.options.password, 'mypass');
         });
     });
   });
 
   describe('fetchRevisions hook', function() {
     it('assigins context.revisions property.', function() {
-      var revisions = [{"revision": "4564564545646"}];
+      var revisions = [ { revision: '4564564545646' } ];
       var client = plugin._client;
       var files = {};
 
-      files["/usr/local/www/my-app/revisions.json"] = JSON.stringify(revisions);
+      files['/usr/local/www/my-app/revisions.json'] = JSON.stringify(revisions);
 
       client._uploadedFiles = files;
 
       var fetching = plugin.fetchRevisions(context);
 
       return assert.isFulfilled(fetching).then(function() {
-        assert.deepEqual(context.revisions, revisions); 
+        assert.deepEqual(context.revisions, revisions);
       });
     });
 
     it('assigins context.revisions proptery to empty array if revistion file not found.', function() {
       var client = plugin._client;
-      
+
       client._readFileError = new Error('No such file');
       client._readFile = null;
 
       var fetching = plugin.fetchRevisions(context);
 
       return assert.isFulfilled(fetching).then(function() {
-        assert.deepEqual(context.revisions, []); 
+        assert.deepEqual(context.revisions, []);
       });
     });
   });
@@ -180,9 +179,9 @@ describe('the deploy plugin object', function() {
     });
 
     it('copies revision to activationDestination if activationStrategy is copy', function() {
-      context.config.ssh2.activationStrategy = "copy";
+      context.config.ssh2.activationStrategy = 'copy';
       plugin.configure(context);
-      
+
       var activating = plugin.activate(context);
       var client = plugin._client;
 
@@ -208,34 +207,21 @@ describe('the deploy plugin object', function() {
 
   describe('upload hook', function() {
     it('updates revisionManifest', function() {
-      var manifestPath = "/usr/local/www/my-app/revisions.json";
-      var revisions = [{"revision": "4564564545646"}];
+      var manifestPath = '/usr/local/www/my-app/revisions.json';
+      var revisions = [ { revision: '4564564545646' } ];
       var client = plugin._client;
       var files = {};
       files[manifestPath] = JSON.stringify(revisions);
 
       client._uploadedFiles = files;
-      
+
       var uploading = plugin.upload(context);
 
       return assert.isFulfilled(uploading).then(function() {
         var manifest = client._uploadedFiles[manifestPath];
-        revisions.unshift({'revision': '89b1d82820a24bfb075c5b43b36f454b'});
-        assert.equal(JSON.stringify(revisions), manifest); 
+        revisions.unshift({ revision: '89b1d82820a24bfb075c5b43b36f454b' });
+        assert.equal(JSON.stringify(revisions), manifest);
       });
-    });
-
-    it('uploads applicationFiles', function() {
-      // var client = plugin._client;
-      // var uploading = plugin.upload(context);
-
-
-      // return assert.isFulfilled(uploading).then(function() {
-        
-      //   var index = client._uploadedFiles['/usr/local/www/my-app/revisions/89b1d82820a24bfb075c5b43b36f454b/index.html']
-
-      //   assert.equal(index, 'indexpage'); 
-      // });
     });
   });
 
